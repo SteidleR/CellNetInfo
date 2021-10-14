4a252e4c7ebf420a0ed806ee64f8d705e0e2011a
vap0r <inf19152@lehre.dhbw-stuttgart.de>
2021-10-14 16:30:45 +0200

    Initial commit

---
+++ app/src/main/java/xyz/steidle/cellularnetworkmapper/MainActivity.java
@@ -0,0 +1,14 @@
+package xyz.steidle.cellularnetworkmapper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
