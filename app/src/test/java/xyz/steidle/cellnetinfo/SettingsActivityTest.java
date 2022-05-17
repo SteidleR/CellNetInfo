27d9b10f90e59b059e6864b94ac8dbac1e2d24cd
vap0r <inf19152@lehre.dhbw-stuttgart.de>
2022-05-17 11:58:45 +0200

    fix issues with gradle and tests

--- app/src/test/java/xyz/steidle/cellnetinfo/SettingsActivityTest.java
+++ app/src/test/java/xyz/steidle/cellnetinfo/SettingsActivityTest.java
@@ -6,12 +6,7 @@
 
 package xyz.steidle.cellnetinfo;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 import android.os.storage.StorageManager;
-import android.os.storage.StorageVolume;
 import android.util.Log;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -24,8 +19,6 @@ import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-
-import java.io.File;
 
 import xyz.steidle.cellnetinfo.utils.DatabaseHandler;
 
@@ -60,22 +53,5 @@ public class SettingsActivityTest {
 
         PowerMockito.verifyStatic(Log.class, Mockito.times(1));
         Log.d(SettingsActivity.LOGTAG, "Empty Preference 'clear_hist'");
-    }
-
-    @Test
-    public void settingsFragmentSetExportHistoryOnClick() {
-
-    }
-
-    @Test
-    public void settingsFragmentCreateStoragePath() {
-        StorageVolume storageVolume = mock(StorageVolume.class);
-        File file = mock(File.class);
-        when(file.getAbsolutePath()).thenReturn("/test");
-        when(storageVolume.getDirectory()).thenReturn(file);
-        when(storageManager.getPrimaryStorageVolume()).thenReturn(storageVolume);
-
-        settingsFragment.SDK_INT = 30;
-        assertEquals("/test/Download/test.csv", settingsFragment.createStoragePath("test.csv"));
     }
 }
